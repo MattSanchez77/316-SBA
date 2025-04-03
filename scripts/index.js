780447e3e44f074590b070d303c7c7d419037f15
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-04-02 23:28:43 -0600

    More changes

--- scripts/index.js
+++ scripts/index.js
@@ -24,20 +24,34 @@ messageOut('Click to Begin');
 function guesser(){
   console.log(myInput.value);
   const val = myInput.value;
+  myInput.value = '';
   game.guess++;
   console.log(game.randomNum == val);
   
   if(game.randomNum == val ){
     console.log('correct');
+    gameOver()
     } else if (game.randomNum > val){
       console.log('Too Low');
       vals.min = val;
+    insetUp();
     } else{
       console.log('Too High');
       vals.mix = val;
-    }
     insetUp();
+    }
 
+}
+
+function gameOver(v){
+  const rep = game.guess == 1 ? 'guess' : 'guesses';
+  messageOut(`You Won! ${v} it took ${game.guess} ${rep}`);
+game.score++;
+game.guess = 0;
+myInput.style.display = 'none';
+btn.style.display = 'none';
+  btnStart.style.display = 'block'
+  btnStart.textContent = 'Play Again!'
 }
 
 function insetUp(){
