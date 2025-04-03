aaa21d387376f027d65e128fd33c15090933b949
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-04-02 22:31:50 -0600

    More Change

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,29 @@ const btn = document.querySelector('button');
 const output = document.querySelector('.results');
 const message = document.createElement('div');
 output.append(message);
+const btnStart = document.createElement('button');
+output.append(btnStart);
+btnStart.textContent = 'Begin Game';
+
 const vals = {min:0,max:50}
 const game = {score:0, randomNum:numberMaker(vals.min,vals.max)};
 myInput.setAttribute('type','number');
 myInput.style.display = 'none';
+btn.style.display = 'none';
 btn.textContent = 'Guess';
-btn.addEventListener('click',starter);
+
+
+btnStart.addEventListener('click',starter);
 messageOut('Click to Begin');
 
 
 function starter(e){
+  vals.min = numberMaker(0,1);
+  vals.max = numberMaker(vals.min+10,vals.min+100);
+  game.randomNum = numberMaker(vals.min,vals.max);
+  myInput.style.display = 'block';
+  btn.style.display = 'block';
+  btnStart.style.display = 'none'
   console.log('start');
   
 }
@@ -24,23 +37,3 @@ function messageOut(mes){
 function numberMaker(min,max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
-
-// const randomNumber = Math.floor(Math.random() * 100) + 1;
-
-// function checkGuess() {
-//     const userGuess = parseInt(document.getElementById("guess").value);
-//     const message = document.getElementById("message");
-
-//     if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
-//         message.textContent = "Please enter a valid number between 1 and 100.";
-//         return;
-//     }
-
-//     if (userGuess === randomNumber) {
-//         message.textContent = "Congratulations! You guessed the correct number!";
-//     } else if (userGuess < randomNumber) {
-//         message.textContent = "Too low! Try again.";
-//     } else {
-//         message.textContent = "Too high! Try again.";
-//     }
-// }
